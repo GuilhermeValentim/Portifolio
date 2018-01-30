5b31f569bfa6cb03ed673a7c30083b1abb464dc8
Guilherme <guilhermevalentim5@gmail.com>
2018-01-30 00:05:07 -0300

    Utilizando o Image viewer for Ionic 2+ para melhor visualização das cartas do Planning Poker

--- src/pages/planning-poker/planning-poker.ts
+++ src/pages/planning-poker/planning-poker.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { NumbersPage } from '../numbers/numbers';
 
 @IonicPage()
 @Component({
@@ -12,47 +11,4 @@ export class PlanningPokerPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  public open0NumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
-
-  public open1NumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
-
-  public open2NumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
-
-  public open3NumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
-
-  public open5NumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
-
-  public open8NumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
-
-  public open13NumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
-
-  public open20NumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
-
-  public open40NumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
-
-  public open100NumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
-
-  public openDoubtNumberPage() {
-    this.navCtrl.push(NumbersPage);
-  }
 }
